25caa21c7ac70d01112728da6662573900cde420
owais6153 <owais6153@gmail.com>
2022-11-04 07:39:13 +0500

    Try to run deployer

--- deployer.sh
+++ deployer.sh
@@ -1,6 +1,8 @@
 set -e
 echo "Deployment Start"
 GIT_SSH_COMMAND="ssh -i ~/.ssh/backend" git pull origin master
+sudo chown -R $USER composer.json
+sudo chown -R $USER composer.lock
 composer update
 php artisan migrate
 echo "Deployment Finished"
