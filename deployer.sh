3d71f84ceb2536da87805891f38d0f76f91f8b76
owais6153 <owais6153@gmail.com>
2022-11-08 15:09:49 -0800

    Deploy Changes

--- deployer.sh
+++ deployer.sh
@@ -1,6 +1,6 @@
 set -e
 echo "Deployment Start"
-GIT_SSH_COMMAND="ssh -i ~/.ssh/backend" git pull origin master
 git checkout .
+GIT_SSH_COMMAND="ssh -i ~/.ssh/backend" git pull origin master
 php artisan migrate
 echo "Deployment Finished"
